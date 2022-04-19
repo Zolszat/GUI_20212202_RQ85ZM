e2550c5be283f105cfe7d3a2b92d784b856824bc
Szatmári Zoltán <zolszat@gmail.com>
2022-04-19 17:24:35 +0200

    OnRender method override created

--- Nikoban/Renderer/Display.cs
+++ Nikoban/Renderer/Display.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Nikoban.Renderer
 {
     public class Display : FrameworkElement
     {
-
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
     }
 }
