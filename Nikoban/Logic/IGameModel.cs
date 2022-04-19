8fce3d6a8d60367a2fecb4625e80dedeea2fcfd3
Szatmári Zoltán <zolszat@gmail.com>
2022-04-19 18:16:10 +0200

    Map property added to IGameModel interface

--- Nikoban/Logic/IGameModel.cs
+++ Nikoban/Logic/IGameModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Nikoban.Logic
+﻿using static Nikoban.Logic.GameLogic;
+
+namespace Nikoban.Logic
 {
     public interface IGameModel
     {
+        GameItem[,] Map { get; set; }
     }
 }
