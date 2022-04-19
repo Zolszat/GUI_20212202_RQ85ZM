06e38cb44575dca7875288be5022ea2681d0807e
Szatmári Zoltán <zolszat@gmail.com>
2022-04-19 17:56:42 +0200

    ConvertToGameItem converter method created

--- Nikoban/Logic/GameLogic.cs
+++ Nikoban/Logic/GameLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +14,23 @@ namespace Nikoban.Logic
             player, wall, floor, box, target // lehetséges elemek a pályán
         }
         public GameItem[,] Map { get; set; }
+        private GameItem ConvertToGameItem(char c)
+        {
+            switch (c)
+            {
+                case '#':
+                    return GameItem.wall;
+                case 'P':
+                    return GameItem.player;
+                case ' ':
+                    return GameItem.floor;
+                case 'B':
+                    return GameItem.box;
+                case 'T':
+                    return GameItem.target;
+                default:
+                    return GameItem.floor;
+            }
+        }
     }
 }
