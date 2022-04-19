cdbbb3c5bca7e5074c8b603b8ebd939df06821b2
Szatmári Zoltán <zolszat@gmail.com>
2022-04-19 17:14:18 +0200

    GameLogic class created

---
+++ Nikoban/Logic/GameLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikoban.Logic
+{
+    public class GameLogic : IGameModel, IGameControl
+    {
+
+    }
+}
