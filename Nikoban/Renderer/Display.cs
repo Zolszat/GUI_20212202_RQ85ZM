1e388914fac7bb54fbf5b3ac9b85e688726c82b5
Szatmári Zoltán <zolszat@gmail.com>
2022-04-21 21:07:30 +0200

    Levels added, ImageBrush added

--- Nikoban/Renderer/Display.cs
+++ Nikoban/Renderer/Display.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Nikoban.Renderer
 {
@@ -24,6 +25,7 @@ namespace Nikoban.Renderer
             {
                 double tileHeight = size.Height / model.Map.GetLength(0);
                 double tileWidth = size.Width / model.Map.GetLength(1);
+                ImageBrush brush = new ImageBrush();
                 for (int i = 0; i < model.Map.GetLength(0); i++)
                 {
                     for (int j = 0; j < model.Map.GetLength(1); j++)
@@ -31,23 +33,24 @@ namespace Nikoban.Renderer
                         switch (model.Map[i,j])
                         {
                             case GameLogic.GameItem.player:
-                                drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1), new Rect(j*tileWidth, i*tileHeight, tileWidth, tileHeight));
+                                brush = new ImageBrush(new BitmapImage(new Uri("Images/shrek_template/character_s.jpg", UriKind.RelativeOrAbsolute)));
                                 break;
                             case GameLogic.GameItem.wall:
-                                drawingContext.DrawRectangle(Brushes.Gray, new Pen(Brushes.Black, 1), new Rect(j * tileWidth, i * tileHeight, tileWidth, tileHeight));
+                                brush = new ImageBrush(new BitmapImage(new Uri("Images/shrek_template/wall_s.jpg", UriKind.RelativeOrAbsolute)));
                                 break;
                             case GameLogic.GameItem.floor:
-                                drawingContext.DrawRectangle(Brushes.White, new Pen(Brushes.Black, 1), new Rect(j * tileWidth, i * tileHeight, tileWidth, tileHeight));
+                                brush = new ImageBrush(new BitmapImage(new Uri("Images/shrek_template/floor_s.jpg", UriKind.RelativeOrAbsolute)));
                                 break;
                             case GameLogic.GameItem.box:
-                                drawingContext.DrawRectangle(Brushes.Brown, new Pen(Brushes.Black, 1), new Rect(j * tileWidth, i * tileHeight, tileWidth, tileHeight));
+                                brush = new ImageBrush(new BitmapImage(new Uri("Images/shrek_template/box_s.jpg", UriKind.RelativeOrAbsolute)));
                                 break;
                             case GameLogic.GameItem.target:
-                                drawingContext.DrawRectangle(Brushes.Red, new Pen(Brushes.Black, 1), new Rect(j * tileWidth, i * tileHeight, tileWidth, tileHeight));
+                                brush = new ImageBrush(new BitmapImage(new Uri("Images/shrek_template/finish_s.jpg", UriKind.RelativeOrAbsolute)));
                                 break;
                             default:
                                 break;
                         }
+                        drawingContext.DrawRectangle(brush, new Pen(Brushes.Black, 1), new Rect(j * tileWidth, i * tileHeight, tileWidth, tileHeight));
                     }
                 }
             }
